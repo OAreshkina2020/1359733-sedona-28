0fd9dc21c3a904db3d7f54b0f33770c82ada3c77
OAreshkina2020 <65291208+OAreshkina2020@users.noreply.github.com>
2020-07-15 14:15:14 +0300

    Последние поправки

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-var appointmentButoon = document.querySelector(".appointment-button");
-var appointmentFormBox = document.querySelector(".input-form");
+var appointmentButton = document.querySelector(".appointment-button");
+var appointmentFormBox = document.querySelector(".appointment-form-box");
 
 appointmentButton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    appointmentFormBox.classList.add("appointment-form-show");
+    appointmentFormBox.classList.toggle("appointment-form-show");
+    arrival.focus();
 });
 
+window.addEventListener("keydown", function (evt) {
+    if(evt.keycode === 27) {
+    if(appointmentFormBox.classList.contains("appointment-form-show")){
+    evt.preventDefault();
+    appointmentFormBox.classList.remove("appointment-form-show");
+    }
+    }
+});
